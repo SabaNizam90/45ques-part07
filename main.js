7e11b974009c529ca8512ddbef9185d35b0cdb7d
Saba Nizam <160767347+SabaNizam90@users.noreply.github.com>
2024-07-09 23:55:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results. 
+console.log(3 + 5); //Addition: 3 + 5 = 8
+console.log(12 - 4); // Subtraction: 12 - 4 = 8
+console.log(2 * 4); //Multiplication: 2 * 4 = 8
+console.log(16 / 2); //Division: 16 / 2 = 8
